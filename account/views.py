2f9bc1a02f93872be1b247f726660992527f2122
HuuHa <huuha84@gmail.com>
2019-11-18 21:00:00 +0700

    register fix commit

--- account/views.py
+++ account/views.py
@@ -1,37 +1,54 @@
-from django.contrib.auth import authenticate, get_user_model, login, logout
-
-from .forms import UserLoginForm, UserRegisterForm
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
 def login_view(request):
-    template='pages/login.html'
-    form = UserLoginForm(request.POST or None)
-    if form.is_valid():
-        username = form.cleaned_data.get("username")
-        password = form.cleaned_data.get("password")
-        user = authenticate(username = username, password = password)
-        login(request,user)
-        return redirect('home')
-    context = {'form':form}
-    return render(request,template,context)
+    template = 'pages/login.html'
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request,user)
+            return redirect('home')
+        else:
+            messages.info(request,'invalid credentials')
+            return redirect('login')
+    else:
+        return render(request,template)
+
 
 def register_view(request):
     template='pages/register.html'
-    form = UserRegisterForm(request.POST or None)
-    if form.is_valid():
-        user = form.save(commit=False)
-        password = form.cleaned_data.get("password")
-        user.set_password
-        user.save()
-        new_user = authenticate(username = user.username, password = password)
-        login(request,new_user)
+    
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        if password1 == password2:
+            if User.objects.filter(username=username).exists():
+                messages.info(request,'tài khoản này đã tồn tại')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request,'email đã tồn tại')
+                return redirect('register')
+            else:
+                new_user = User.objects.create_user(username=username,password=password1,email=email,first_name=first_name,last_name=last_name)
+                new_user.save()
+                messages.info(request,'Đăng ký tài khoản thành công!')
+                return redirect('login')
+        else:
+            messages.info(request,'mật khẩu không đúng...')
+            return redirect('register')
         return redirect('home')
-    context={
-        'form':form,
-    }
-    return render(request,template,context)
+    else: 
+        return render(request,template)
 
 def logout_view(request):
-    logout(request)
-    return redirect("/")
+    auth.logout(request)
+    return redirect('home')
