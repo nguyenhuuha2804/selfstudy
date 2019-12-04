a8200c1598c03e67e8c8f309be3942a9c0580910
HuuHa <huuha84@gmail.com>
2019-12-04 15:56:53 +0700

    update 4/12

--- selfstudy/settings.py
+++ selfstudy/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 # Application definition
 
 INSTALLED_APPS = [
-    'selfstudy.apps.SuitConfig',
+    #'selfstudy.apps.SuitConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.sites',
